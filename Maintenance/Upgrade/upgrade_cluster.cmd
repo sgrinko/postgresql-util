1d36b8ce45e5d2fde8fc9dfe152d764f7434b21d
sgrinko <grufos@mail.ru>
2016-03-09 13:22:29 +0300

    Автоподстановка текущего каталога при вызове основно cmd файла

--- Maintenance/Upgrade/upgrade_cluster.cmd
+++ Maintenance/Upgrade/upgrade_cluster.cmd
@@ -4,7 +4,8 @@ REM обновление с версии 9.4 до версии 9.5
 REM устанавливаем новую версию сервера на порт 5433 и новый DATA каталог D:/PostgresData_9.5
 REM эта часть вручную...
 REM 
-RUNAS /USER:postgres "D:\Backup\PostgreSQL\upgrade_cluster_process.cmd"
+SET PATH_CURRENT=%~dp0
+echo RUNAS /USER:postgres "%PATH_CURRENT%upgrade_cluster_process.cmd"
 
 
 
